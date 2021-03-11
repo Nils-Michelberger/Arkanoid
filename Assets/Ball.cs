c708d26475d1c477c33ba0fbe48d356294c88604
Nils Michelberger <nmichelb@stud.hs-heilbronn.de>
2021-03-11 20:07:25 +0100

    Finished GameScene and implemented Menus + UI Elements

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -1,13 +1,20 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Ball : MonoBehaviour
 {
     private Vector3 velocity;
     public float speedX;
     public float speedZ;
+    public TextMeshProUGUI health;
+    public TextMeshProUGUI score;
+    private int scoreValue = 0;
+    private int healthValue = 3;
     
     // Start is called before the first frame update
     void Start()
@@ -36,8 +43,28 @@ public class Ball : MonoBehaviour
         }
         else if (other.CompareTag("Block"))
         {
+            scoreValue += 10;
+            score.text = scoreValue.ToString();
             velocity = new Vector3(velocity.x, velocity.y, -velocity.z);
         }
+        else if (other.CompareTag("Border"))
+        {
+            healthValue--;
+            if (healthValue <= 0) 
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+            health.text = healthValue.ToString();
+            StartCoroutine(ResetBall());
+        }
         GetComponent<AudioSource>().Play();
+    }
+
+    private IEnumerator ResetBall()
+    {
+        velocity = new Vector3(0, 0, 0);
+        transform.position = new Vector3(0, 0.5f, -2f);
+        yield return new WaitForSeconds(2f);
+        velocity = new Vector3(0, 0, -speedZ);
     }
 }
