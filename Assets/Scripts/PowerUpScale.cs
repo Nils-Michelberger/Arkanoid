f346187824b84630ac6a76a08e2e468e56500a35
Nils Michelberger <nmichelb@stud.hs-heilbronn.de>
2021-03-15 17:24:54 +0100

    Added paddle plinking when scaling powerup is running out

--- Assets/Scripts/PowerUpScale.cs
+++ Assets/Scripts/PowerUpScale.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class PowerUpScale : Powerup
 {
+    public Material redMaterial;
+    
     private void OnTriggerEnter(Collider other)
     {
         ToggleVisibilityAndCollider(false);
@@ -18,11 +20,32 @@ public class PowerUpScale : Powerup
 
     private IEnumerator PowerUpActive(Paddle paddle)
     {
-        yield return new WaitForSeconds(15f);
+        yield return new WaitForSeconds(13f);
+
+        StartCoroutine(BlinkPaddle());
+        yield return new WaitForSeconds(2f);
 
         var localScalePaddle = paddle.transform.localScale;
         paddle.transform.localScale = new Vector3(localScalePaddle.x / 2f, localScalePaddle.y, localScalePaddle.z);
         
         StartTimerForPowerUp();
+    }
+    
+    private IEnumerator BlinkPaddle()
+    {
+        Paddle paddle = GameObject.Find("Paddle").GetComponent<Paddle>();
+        Material blueMaterial = paddle.GetComponent<Renderer>().sharedMaterial;
+
+        paddle.GetComponent<Renderer>().sharedMaterial = redMaterial;
+        yield return new WaitForSeconds(0.4f);
+        paddle.GetComponent<Renderer>().sharedMaterial = blueMaterial;
+        yield return new WaitForSeconds(0.4f);
+        paddle.GetComponent<Renderer>().sharedMaterial = redMaterial;
+        yield return new WaitForSeconds(0.4f);
+        paddle.GetComponent<Renderer>().sharedMaterial = blueMaterial;
+        yield return new WaitForSeconds(0.4f);
+        paddle.GetComponent<Renderer>().sharedMaterial = redMaterial;
+        yield return new WaitForSeconds(0.4f);
+        paddle.GetComponent<Renderer>().sharedMaterial = blueMaterial;
     }
 }
